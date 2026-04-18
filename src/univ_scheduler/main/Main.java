85f5167acb09c031a2c9c58d04be1b180b383c0b
Aminata Toure <minatatoure2008@gmail.com>
2026-04-18 19:19:55 +0000

    version final du projet

--- src/univ_scheduler/main/Main.java
+++ src/univ_scheduler/main/Main.java
@@ -18,6 +18,7 @@ public class Main {
 	public static void main(String[] args) {
 		 SwingUtilities.invokeLater(new Runnable() {
 	            public void run() {
+	            	
 	                new LoginView().setVisible(true);
 	            }
 	        });
